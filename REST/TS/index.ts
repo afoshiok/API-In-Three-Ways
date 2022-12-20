1d5a169bd3583dd32518f704a726c7a9bc885bb6
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2022-12-20 13:40:51 -0600

    Update index.ts

--- REST/TS/index.ts
+++ REST/TS/index.ts
@@ -9,6 +9,14 @@ const port = process.env.PORT;
 
 const prisma = new PrismaClient()
 
+//GET REQUESTS
+
+//GET all Barbers
+app.get("/barber", async (req: Request, res: Response) => {
+    const barbers = await prisma.barber.findMany()
+    res.json(barbers)
+})
+
 app.listen(port, () => {
     console.log(`Your REST API is running on https://localhost:${port} 👍`)
 })
